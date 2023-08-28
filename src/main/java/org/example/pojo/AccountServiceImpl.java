a5778f07f4c63876e76b164e11f3d6ea8de44a9c
chung130196 <nguyenngocchung14@gmail.com>
2023-08-28 21:05:29 +0700

    commit

---
+++ src/main/java/org/example/pojo/AccountServiceImpl.java
@@ -0,0 +1,31 @@
+package org.example.pojo;
+
+public class AccountServiceImpl implements AccountService{
+
+    private AccountRepository accountRepository;
+    public void setAccountRepository(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+    @Override
+    public void transferMoney(long fromAccountId, long toAccountId, double amount) {
+            Account soureceAccount = accountRepository.find(fromAccountId);
+            Account targetAccount = accountRepository.find(toAccountId);
+            soureceAccount.setBalance(soureceAccount.getBalance() - amount);
+            targetAccount.setBalance(targetAccount.getBalance()+amount);
+            accountRepository.update(soureceAccount);
+            accountRepository.update(targetAccount);
+        }
+
+
+    @Override
+    public void depositMoney(long accountId, double amount) throws Exception {
+        Account account = accountRepository.find(accountId);
+        account.setBalance(account.getBalance()+amount);
+        accountRepository.update(account);
+    }
+
+    @Override
+    public Account getAccount(long accountId) {
+            return accountRepository.find(accountId);
+        }
+}
