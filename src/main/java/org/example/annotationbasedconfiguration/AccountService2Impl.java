a415000671f762325d327244517869ff1aa548af
chung130196 <nguyenngocchung14@gmail.com>
2023-09-04 15:48:54 +0700

    commit

---
+++ src/main/java/org/example/annotationbasedconfiguration/AccountService2Impl.java
@@ -0,0 +1,33 @@
+package org.example.annotationbasedconfiguration;
+
+import org.example.pojo.Account;
+import org.example.pojo.AccountRepository;
+import org.example.pojo.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("accountService")
+public class AccountService2Impl implements AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public void transferMoney (long fromAccountId, long toAccountId, double amount){
+        Account sourceAccount = accountRepository.find(fromAccountId);
+        Account targetAccount = accountRepository.find(toAccountId);
+        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+        targetAccount.setBalance(sourceAccount.getBalance() + amount);
+        accountRepository.update(sourceAccount);
+        accountRepository.update(targetAccount);
+    }
+    @Override
+    public void depositMoney (long accountId, double amount) throws Exception{
+        Account account = accountRepository.find(accountId);
+        account.setBalance(account.getBalance() + amount);
+        accountRepository.update(account);
+    }
+    @Override
+    public Account getAccount(long accountId){
+        return accountRepository.find(accountId);
+    }
+}
